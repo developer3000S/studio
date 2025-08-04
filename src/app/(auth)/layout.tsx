69e7d033272ab15cd1e1b79abbcc92774aac4b0e
Alex Tihomirov <medprivate@gmail.com>
2025-08-04 10:44:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -20,7 +20,7 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
         return (
             <div className="flex items-center justify-center min-h-screen">
                 <Loader2 className="h-8 w-8 animate-spin" />
-            </div>>
+            </div>
         )
     }
 
